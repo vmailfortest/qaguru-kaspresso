9a3c663cc02209cda60d9b72296e6a2895ffface
Tester <tester@test.com>
2024-01-22 15:15:32 +0200

    Add allure support.

--- app/src/androidTest/java/qa/guru/kaspresso/DecrementButtonTest.kt
+++ app/src/androidTest/java/qa/guru/kaspresso/DecrementButtonTest.kt
@@ -1,17 +1,26 @@
 package qa.guru.kaspresso
 
+import android.Manifest
 import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.rule.GrantPermissionRule
+import com.kaspersky.components.alluresupport.withForcedAllureSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import org.junit.Rule
 import org.junit.Test
 
 class DecrementButtonTest : TestCase(
-    kaspressoBuilder = Kaspresso.Builder.simple()
+    kaspressoBuilder = Kaspresso.Builder.withForcedAllureSupport()
 ) {
 
     @get:Rule
     val activityRule = activityScenarioRule<MainActivity>()
+
+    @get:Rule
+    val runtimePermission: GrantPermissionRule = GrantPermissionRule.grant(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
 
     @Test
     fun incrementButtonShouldIncreaseCounterValue() = run {
