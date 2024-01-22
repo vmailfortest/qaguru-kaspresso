8930d136f307d1129289ccea207998c4554511e5
Tester <tester@test.com>
2024-01-22 11:30:07 +0200

    Add increment/decrement kaspresso tests.

---
+++ app/src/androidTest/java/qa/guru/kaspresso/IncrementButtonTest.kt
@@ -0,0 +1,33 @@
+package qa.guru.kaspresso
+
+import androidx.test.ext.junit.rules.activityScenarioRule
+import com.kaspersky.kaspresso.kaspresso.Kaspresso
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.junit.Rule
+import org.junit.Test
+
+class IncrementButtonTest : TestCase(
+    kaspressoBuilder = Kaspresso.Builder.simple()
+) {
+
+    @get:Rule
+    val activityRule = activityScenarioRule<MainActivity>()
+
+    @Test
+    fun incrementButtonShouldIncreaseCounterValue() = run {
+
+        step("Increase counter value to 2"){
+            MainScreen {
+                incrementButton.click()
+                incrementButton.click()
+            }
+        }
+
+        step("Counter value should be 2"){
+            MainScreen {
+                counterText.hasText("Count: 2")
+            }
+        }
+
+    }
+}
