413a21979e2bdffe1801a7207dd69f9e480bd262
Tester <tester@test.com>
2024-01-22 11:25:02 +0200

    Add gradle dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,13 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
     testImplementation("junit:junit:4.13.2")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit-ktx:1.1.5")
+
+    androidTestImplementation("io.qameta.allure:allure-kotlin-android:2.4.0")
+    androidTestImplementation("com.kaspersky.android-components:kaspresso:1.5.3")
+    androidTestImplementation("com.kaspersky.android-components:kaspresso-allure-support:1.5.3")
 }
